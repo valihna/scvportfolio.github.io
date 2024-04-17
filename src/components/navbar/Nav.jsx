b24fc426e5352ab89f3a8b95424133ed1fcf31cd
Valeriane SUDRE CHEVALIER <valeriane.chevalier@gmail.com>
2024-04-17 16:10:01 +0200

    ajout des composants du dossier navbar et installation de reacticons react-use framer-motion hamburger-react tailwind

---
+++ src/components/navbar/Nav.jsx
@@ -0,0 +1,24 @@
+import { routes } from "../routes";
+
+const Nav = () => {
+  return (
+    <ul className="hidden lg:flex lg:items-center gap-5 text-sm">
+      {routes.map((route, idx) => {
+        const { Icon, link, title } = route;
+        return (
+          <li key={idx}>
+            <a
+              href={link}
+              className="flex items-center gap-1 hover:text-neutral-400 transition-all"
+            >
+              <Icon />
+              {title}
+            </a>
+          </li>
+        );
+      })}
+    </ul>
+  );
+};
+
+export default Nav;
---
+++ src/components/navbar/routes.jsx
@@ -0,0 +1,34 @@
+import { BiHomeAlt2 } from "react-icons/bi";
+import { FiSearch } from "react-icons/fi";
+import { PiChatCircleBold } from "react-icons/pi";
+import { IoPricetagsOutline } from "react-icons/io5";
+
+export const routes = [
+  {
+    title: "Home",
+    link: "/home",
+    Icon: BiHomeAlt2,
+  },
+  {
+    title: "About",
+    link: "/about",
+    Icon: FiSearch,
+  },
+  {
+    title: "Projects",
+    link: "/projects",
+    Icon: IoPricetagsOutline,
+  },
+  {
+    title: "Skills",
+    link: "/skills",
+    Icon: PiChatCircleBold,
+  },
+  {
+    title: "Contact",
+    link: "/contact",
+    Icon: PiChatCircleBold,
+  },
+];
+
+// export default routes;
