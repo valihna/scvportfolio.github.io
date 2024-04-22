c8e5c5a07920401258c7a20c9888a208a14973ed
Valeriane SUDRE CHEVALIER <valeriane.chevalier@gmail.com>
2024-04-22 19:36:16 +0200

    featdeploy se transforme en responsive et cest bien mieux git add .git add .

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,6 +1,3 @@
-/* eslint-disable no-unused-vars */
-import React from "react";
-
 import { Link } from "react-router-dom";
 import linkedIn from "../../assets/linkedIn.png";
 import github from "../../assets/github.jpg";
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -13,53 +13,62 @@ const Navbar = () => {
   useClickAway(ref, () => setOpen(false));
 
   return (
-    <div className="nav">
-      <div ref={ref} className="lghidden">
-        <Hamburger toggled={isOpen} right={10} size={20} toggle={setOpen} />
-        <AnimatePresence>
-          {isOpen && (
-            <motion.div
-              initial={{ opacity: 0 }}
-              animate={{ opacity: 1 }}
-              exit={{ opacity: 0 }}
-              transition={{ duration: 0.2 }}
-              className="fixed left-0 shadow-4xl right-0 top-[3.5rem] p-5 pt-0 bg-neutral-950 border-b border-b-white/20"
-            >
-              <ul className="grid gap-2">
-                {routes.map((route, idx) => {
-                  const { Icon, link, title } = route;
+    <div className="global-nav">
+      <div className="nav">
+        <div ref={ref} className="lghidden">
+          <Hamburger toggled={isOpen} right={10} size={20} toggle={setOpen} />
+          <AnimatePresence>
+            {isOpen && (
+              <motion.div
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                exit={{ opacity: 0 }}
+                transition={{ duration: 0.2 }}
+                className="fixed left-0 shadow-4xl right-0 top-[3.5rem] p-5 pt-0 bg-neutral-950 border-b border-b-white/20"
+              >
+                <ul className="grid gap-2">
+                  {routes.map((route, idx) => {
+                    const { Icon, link, title } = route;
 
-                  return (
-                    <motion.li
-                      key={title}
-                      initial={{ scale: 0, opacity: 0 }}
-                      animate={{ scale: 1, opacity: 1 }}
-                      transition={{
-                        type: "spring",
-                        stiffness: 260,
-                        damping: 20,
-                        delay: 0.1 + idx / 10,
-                      }}
-                      className="w-full p-[0.08rem] rounded-xl bg-gradient-to-tr from-neutral-800 via-neutral-950 to-neutral-700"
-                    >
-                      <a
-                        onClick={() => setOpen((prev) => !prev)}
-                        className="flex items-center justify-between w-full p-5 rounded-xl bg-neutral-950"
-                        href={link}
+                    return (
+                      <motion.li
+                        key={title}
+                        initial={{ scale: 0, opacity: 0 }}
+                        animate={{ scale: 1, opacity: 1 }}
+                        transition={{
+                          type: "spring",
+                          stiffness: 260,
+                          damping: 20,
+                          delay: 0.1 + idx / 10,
+                        }}
+                        className="w-full p-[0.08rem] rounded-xl bg-gradient-to-tr from-neutral-800 via-neutral-950 to-neutral-700"
                       >
-                        <span className="flex gap-1 text-lg">{title}</span>
-                        <Icon className="text-xl" />
-                      </a>
-                    </motion.li>
-                  );
-                })}
-              </ul>
-            </motion.div>
-          )}
-        </AnimatePresence>
-      </div>
-      <div className="logo">
-        <img className="logo" src={logo} alt="logo" />
+                        <a
+                          onClick={() => setOpen((prev) => !prev)}
+                          className="flex items-center justify-between w-full p-5 rounded-xl bg-neutral-950"
+                          href={link}
+                        >
+                          <span className="flex gap-1 text-lg">{title}</span>
+                          <Icon className="text-xl" />
+                        </a>
+                      </motion.li>
+                    );
+                  })}
+                </ul>
+              </motion.div>
+            )}
+          </AnimatePresence>
+        </div>
+        <div className="slogan">
+          <h2>
+            VSC Web - Transform your vision into an exceptional web experience.
+            {/* VSC Web - Transformez votre vision en une expérience web
+            exceptionnelle. */}
+          </h2>
+        </div>
+        <div className="logo">
+          <img className="logo" src={logo} alt="logo" />
+        </div>
       </div>
     </div>
   );
