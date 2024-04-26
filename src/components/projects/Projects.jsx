5f688592418a20d8a026cb6c70ec0b82210a152c
Valeriane SUDRE CHEVALIER <valeriane.chevalier@gmail.com>
2024-04-26 15:28:32 +0200

    tout etait trop grand donc remise a niveau du css

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -1,6 +1,5 @@
 /* eslint-disable no-unused-vars */
 import React from "react";
-import { Link } from "react-router-dom";
 import html from "../../assets/html.png";
 import css from "../../assets/css.png";
 import js from "../../assets/js.png";
@@ -16,165 +15,180 @@ function Projects() {
     <div className="projects">
       <h2>Projects</h2>
       <br />
-      <div className="project-cards">
-        <div className="project-name">
-          <h3>Eating Nam Nam</h3>
-        </div>
-        <div className="imageSite">
-          <Link to="https://youtu.be/wdYOhhNVyps" target="_blank">
-            <p className="deployed">
-              Cliquer sur ce lien pour accéder à la vidéo youtube du site
-            </p>
-          </Link>
-          <div className="langagesOutils">
-            <h3> Stack </h3>
-            <div className="stack">
-              <li>
-                <img src={react} alt="Logo React" />
-              </li>
-              <li>
-                <img src={express} alt="Logo Express" />
-              </li>
-              <li>
-                <img src={git} alt="Logo Git" />
-              </li>
-              <li>
-                <img src={html} alt="Logo HTML" />
-              </li>
-              <li>
-                <img src={css} alt="Logo css" />
-              </li>
-              <li>
-                <img src={js} alt="Logo JavaScript" />
-              </li>
-              <li>
-                <img src={mysql} alt="Logo MySQL" />
-              </li>
-              <li>
-                <img src={github} alt="Logo GitHub" />
-              </li>
+      <div className="global-projects">
+        <div className="project-cards">
+          <div className="project-name">
+            <h3>Eating Nam Nam</h3>
+          </div>
+          <div className="imageSite">
+            <iframe
+              width="100%"
+              height="315"
+              src="https://www.youtube.com/embed/wdYOhhNVyps"
+              title="Eating Nam Nam Video"
+              frameBorder="0"
+              allowfullscreen
+            ></iframe>
+            <div className="langages-outils">
+              <h3> Stack </h3>
+              <div className="stack">
+                <li>
+                  <img src={react} alt="Logo React" />
+                </li>
+                <li>
+                  <img src={express} alt="Logo Express" />
+                </li>
+                <li>
+                  <img src={git} alt="Logo Git" />
+                </li>
+                <li>
+                  <img src={html} alt="Logo HTML" />
+                </li>
+                <li>
+                  <img src={css} alt="Logo css" />
+                </li>
+                <li>
+                  <img src={js} alt="Logo JavaScript" />
+                </li>
+                <li>
+                  <img src={mysql} alt="Logo MySQL" />
+                </li>
+                <li>
+                  <img src={github} alt="Logo GitHub" />
+                </li>
+              </div>
             </div>
           </div>
         </div>
-      </div>
-      <br />
-      <div className="project-cards">
-        <div className="project-name">
-          <h3>Cent Pression</h3>
-        </div>
-        <div className="imageSite">
-          <Link to="https://youtu.be/MbkW-HWiLns" target="_blank">
-            <p className="deployed">
-              Cliquer sur ce lien pour accéder à la vidéo youtube du site
-            </p>
-          </Link>
-          <div className="langagesOutils">
-            <h3> Stack </h3>
-            <div className="stack">
-              <li>
-                <img src={react} alt="Logo React" />
-              </li>
-              <li>
-                <img src={express} alt="Logo Express" />
-              </li>
-              <li>
-                <img src={git} alt="Logo Git" />
-              </li>
-              <li>
-                <img src={html} alt="Logo HTML" />
-              </li>
-              <li>
-                <img src={css} alt="Logo css" />
-              </li>
-              <li>
-                <img src={js} alt="Logo JavaScript" />
-              </li>
-              <li>
-                <img src={mysql} alt="Logo MySQL" />
-              </li>
-              <li>
-                <img src={github} alt="Logo GitHub" />
-              </li>
+        <br />
+        <div className="project-cards">
+          <div className="project-name">
+            <h3>Cent Pression</h3>
+          </div>
+          <div className="imageSite">
+            <iframe
+              width="100%"
+              height="315"
+              src="https://www.youtube.com/embed/MbkW-HWiLns"
+              title="Cent Pression Video"
+              frameBorder="0"
+              allowfullscreen
+            ></iframe>
+            <div className="langages-outils">
+              <h3> Stack </h3>
+              <div className="stack">
+                <li>
+                  <img src={react} alt="Logo React" />
+                </li>
+                <li>
+                  <img src={express} alt="Logo Express" />
+                </li>
+                <li>
+                  <img src={git} alt="Logo Git" />
+                </li>
+                <li>
+                  <img src={html} alt="Logo HTML" />
+                </li>
+                <li>
+                  <img src={css} alt="Logo css" />
+                </li>
+                <li>
+                  <img src={js} alt="Logo JavaScript" />
+                </li>
+                <li>
+                  <img src={mysql} alt="Logo MySQL" />
+                </li>
+                <li>
+                  <img src={github} alt="Logo GitHub" />
+                </li>
+              </div>
             </div>
           </div>
         </div>
-      </div>
-      <br />
-      <div className="project-cards">
-        <div className="project-name">
-          <h3>Hakathon Loréal</h3>
-        </div>
-        <div className="imageSite">
-          <Link to="https://youtu.be/HTD-Qbt0Gok" target="_blank">
-            <p className="deployed">
-              Cliquer sur ce lien pour accéder à la vidéo youtube du site
-            </p>
-          </Link>
-          <div className="langagesOutils">
-            <h3> Stack </h3>
-            <div className="stack">
-              <li>
-                <img src={react} alt="Logo React" />
-              </li>
-              <li>
-                <img src={express} alt="Logo Express" />
-              </li>
-              <li>
-                <img src={git} alt="Logo Git" />
-              </li>
-              <li>
-                <img src={html} alt="Logo HTML" />
-              </li>
-              <li>
-                <img src={css} alt="Logo css" />
-              </li>
-              <li>
-                <img src={js} alt="Logo JavaScript" />
-              </li>
-              <li>
-                <img src={mysql} alt="Logo MySQL" />
-              </li>
-              <li>
-                <img src={github} alt="Logo GitHub" />
-              </li>
+        <br />
+        <div className="project-cards">
+          <div className="project-name">
+            <h3>Hakathon Loréal</h3>
+          </div>
+          <div className="imageSite">
+            <iframe
+              width="100%"
+              height="315"
+              src="https://www.youtube.com/embed/HTD-Qbt0Gok"
+              title="Hakathon Loréal Video"
+              frameBorder="0"
+              allowfullscreen
+            ></iframe>
+            <div className="langages-outils">
+              <h3> Stack </h3>
+              <div className="stack">
+                <li>
+                  <img src={react} alt="Logo React" />
+                </li>
+                <li>
+                  <img src={express} alt="Logo Express" />
+                </li>
+                <li>
+                  <img src={git} alt="Logo Git" />
+                </li>
+                <li>
+                  <img src={html} alt="Logo HTML" />
+                </li>
+                <li>
+                  <img src={css} alt="Logo css" />
+                </li>
+                <li>
+                  <img src={js} alt="Logo JavaScript" />
+                </li>
+                <li>
+                  <img src={mysql} alt="Logo MySQL" />
+                </li>
+                <li>
+                  <img src={github} alt="Logo GitHub" />
+                </li>
+              </div>
             </div>
           </div>
         </div>
-      </div>
-      <div className="project-cards">
-        <div className="project-name">
-          <h3>Quiz Biarritz</h3>
-        </div>
-        <div className="imageSite">
-          <Link to="https://youtu.be/5klnBXn97fU" target="_blank">
-            <p className="deployed">
-              Cliquer sur ce lien pour accéder à la vidéo youtube du site
-            </p>
-          </Link>
-          <div className="langagesOutils">
-            <h3> Stack </h3>
-            <div className="stack">
-              <li>
-                <img src={react} alt="Logo React" />
-              </li>
-              <li>
-                <img src={express} alt="Logo Express" />
-              </li>
-              <li>
-                <img src={git} alt="Logo Git" />
-              </li>
-              <li>
-                <img src={html} alt="Logo HTML" />
-              </li>
-              <li>
-                <img src={css} alt="Logo css" />
-              </li>
-              <li>
-                <img src={js} alt="Logo JavaScript" />
-              </li>
-              <li>
-                <img src={github} alt="Logo GitHub" />
-              </li>
+        <br />
+        <div className="project-cards">
+          <div className="project-name">
+            <h3>Quiz Biarritz</h3>
+          </div>
+          <div className="imageSite">
+            <iframe
+              width="100%"
+              height="315"
+              src="https://www.youtube.com/embed/5klnBXn97fU"
+              title="Quiz Biarritz Video"
+              frameBorder="0"
+              allowfullscreen
+            ></iframe>
+            <div className="langages-outils">
+              <h3> Stack </h3>
+              <div className="stack">
+                <li>
+                  <img src={react} alt="Logo React" />
+                </li>
+                <li>
+                  <img src={express} alt="Logo Express" />
+                </li>
+                <li>
+                  <img src={git} alt="Logo Git" />
+                </li>
+                <li>
+                  <img src={html} alt="Logo HTML" />
+                </li>
+                <li>
+                  <img src={css} alt="Logo css" />
+                </li>
+                <li>
+                  <img src={js} alt="Logo JavaScript" />
+                </li>
+                <li>
+                  <img src={github} alt="Logo GitHub" />
+                </li>
+              </div>
             </div>
           </div>
         </div>
